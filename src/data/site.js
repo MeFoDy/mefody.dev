0b740a2052f9f0f0a1b2d5ad76a7e5df1143e9f0
mefody <n.a.dubko@gmail.com>
2021-03-28 04:35:26 +0300

    Add OG-tags

--- src/data/site.js
+++ src/data/site.js
@@ -1,12 +1,11 @@
 module.exports = {
-    title: 'TODO',
-    ogTitle: 'TODO',
-    description: 'TODO',
+    title: 'Dev Tips',
+    description: 'Hello, my name is Nikita and I am a front-end developer. Here are life hacks and tips related to my daily work.',
     twitter: 'dev_tip',
     paths: {
         site: 'https://tips.mefody.dev',
         feed: 'https://tips.mefody.dev/feed/',
-        social: 'https://tips.mefody.dev/images/social.png',
+        social: 'https://tips.mefody.dev/images/favicon/logo-tips-192.png',
         github: 'https://github.com/MeFoDy/tips.mefody.dev',
     },
     buildYear: new Date().getFullYear(),
