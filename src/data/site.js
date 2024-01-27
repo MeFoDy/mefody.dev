3c7f235fdcd0ae8724810d3fb4e82c21c708fc71
mefody <mefody@yandex-team.ru>
2024-01-27 13:35:44 +0300

    Add Mastodon account

--- src/data/site.js
+++ src/data/site.js
@@ -1,8 +1,10 @@
 module.exports = {
     title: 'Dev Tips by Nikita Dubko',
     author: 'Nikita Dubko',
-    description: 'Hello, my name is Nikita and I am a front-end developer. Here are life hacks and tips related to my daily work.',
+    description:
+        'Hello, my name is Nikita and I am a front-end developer. Here are life hacks and tips related to my daily work.',
     twitter: 'dev_tip',
+    mastodon: 'dev_tip',
     paths: {
         domain: 'mefody.dev',
         site: 'https://mefody.dev',
